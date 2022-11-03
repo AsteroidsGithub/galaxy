6c275d083cbc711951a878eef6feee43a1af9f1c
Jonathan Voss <jv96@mahurangi.school.nz>
2022-11-03 23:49:16 +1300

    register: removed redundant code

--- register.ts
+++ register.ts
@@ -1,23 +1,23 @@
-
-import fs from "fs"
-import { REST, Routes } from "discord.js"
-import "dotenv/config"
-import { GalaxyCommand } from "./interfaces/galaxy";
+import { GalaxyCommand } from './interfaces/galaxy';
+import { REST, Routes } from 'discord.js';
+import 'dotenv/config';
+import fs from 'fs';
 
 (async () => {
-    if (!process.env.TOKEN) throw new Error("No token provided");
-    if (!process.env.GUILD_ID) throw new Error("No guild id provided");
-    if (!process.env.CLIENT_ID) throw new Error("No client id provided");
-
-    const commands = [];
-    // Grab all the command files from the commands directory you created earlier
-    const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.ts'));
+    if (!process.env.TOKEN) throw new Error('No token provided');
+    if (!process.env.GUILD_ID) throw new Error('No guild id provided');
+    if (!process.env.CLIENT_ID) throw new Error('No client id provided');
 
     // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-    for (const file of commandFiles) {
-        const command: GalaxyCommand = await import(`./commands/${file}`).then((module) => module.default);
-        commands.push(command.data.toJSON());
-    }
+    const commands = fs
+        .readdirSync('./commands')
+        .filter((file) => file.endsWith('.ts'))
+        .map(async (file) => {
+            const command: GalaxyCommand = await import(`../commands/${file}`).then(
+                (module) => module.default
+            );
+            return command.data.toJSON();
+        });
 
     // Construct and prepare an instance of the REST module
     const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
@@ -27,9 +27,9 @@ import { GalaxyCommand } from "./interfaces/galaxy";
         console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
         // The put method is used to fully refresh all commands in the guild with the current set
-        const data = await rest.put(
+        await rest.put(
             Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
-            { body: commands },
+            { body: commands }
         );
 
         console.log(`Successfully reloaded application (/) commands.`);
